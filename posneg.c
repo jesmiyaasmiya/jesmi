3c510168f095f2410afb2cc6d5bc7a8080e06574
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-08 16:49:46 +0530

    Create posneg.c

---
+++ posneg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+printf("enter the number:%d",num);
+scanf("%d",&num);
+if (num > 0)
+printf("%d is a positive number",num);
+else if(num < 0)
+printf("%d is a negative number",num);
+else
+printf("%d is zero",num);
+return 0;
+}
