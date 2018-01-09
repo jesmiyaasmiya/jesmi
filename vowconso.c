1eb163c328dfae5fa9e6b00ce19e2f72758f5a67
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-09 18:58:20 +0530

    Create vowconso.c

---
+++ vowconso.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char let;
+printf("enter the letter:",let);
+scanf("%c",&let);
+printf("enter the letter");
+{
+if(let == 'a')
+printf("%c is a vowel",let);
+else if(let == 'e')
+printf("%c is a vowel",let);
+else if(let == 'i')
+printf("%c is a vowel",let);
+else if(let == 'o')
+printf("%c is a vowel",let);
+else if(let == 'u')
+printf("%c is a vowel",let);
+else
+printf("%c is a consonant");
+}
