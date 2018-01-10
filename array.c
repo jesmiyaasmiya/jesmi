dbc796468aca947c6823364b546b305896e90d72
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-10 16:41:22 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,13 @@
+include<stdio.h>
+int main()
+{
+int n,i,sum=0;
+printf("enter number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum =sum +1;
+}
+printf("sum of n number is :%d",sum);
+return 0;
+}
