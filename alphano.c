22379a44e9aeec07b9b43a09f81319347ef24a33
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-10 15:57:49 +0530

    Update alphano.c

--- alphano.c
+++ alphano.c
@@ -6,7 +6,7 @@ char ch;
 printf("enter the letter:",ch);
 scanf("%c",&ch);
 {
-if (ch > 65 && ch < 90) || (ch > 92 && ch < 122)
+if ((ch >= 65 && ch <= 90) || (ch >= 92 && ch <= 122))
 printf("%c is a alphabet",ch);
 else
 printf("%c is not a alphabet",ch);
