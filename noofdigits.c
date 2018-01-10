25e4739ddaf46acb2de43d0a9a534440d64bb7ae
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-10 16:35:53 +0530

    Update noofdigits.c

--- noofdigits.c
+++ noofdigits.c
@@ -3,11 +3,12 @@ int main()
 {
 int n, count=0;
 printf("%d",n);
-while ( n != 0 )
+scanf("%d",&n);
+while(n!=0)
 {
 n=n/10;
 count++;
 }
-printf("no of digits:",count);
+printf("no of digits:%d",count);
 return 0;
-}_
+}
