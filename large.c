d0435badf63118b633c4f2732a2ac515a2d6df46
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-10 15:42:12 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter three numbers:",a,b,c);
+scanf("%d%d%d",&a,&b,&c);
+{
+if ( a > b ||  a > c )
+printf("%d is larger",a);
+else if (b > a || b > c)
+printf("%d is larger",b);
+else if (c > a || c > b)
+printf("%d is larger",c);
+}
+}
