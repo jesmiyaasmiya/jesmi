1994a037250ed07cba0b77ac0847203e572d7e14
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-19 18:50:26 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int fact=1,i,n;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
+	int num,i,fact=1;
+	printf("enter the number",num);
+	scanf("%d",&num);
+	for(i=1;i<=num;i++)
 	{
-	fact=fact*i;
-}
-printf("factorial is %d",fact);
+		fact=fact*i;
+	}
+	printf("factorial is %d",fact);
 	return 0;
 }
