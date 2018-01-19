c1aa08b2e4d1787ddbac5105bf649f2766f94e15
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-19 16:55:58 +0530

    Create armstronginterval.c

---
+++ armstronginterval.c
@@ -0,0 +1,24 @@
+include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,t,a=150,b=160,rem,result=0;
+printf("the first interval %d",a);
+printf("the second interval %d",b);
+{
+for(i=150;i<=160;i++)
+{
+t=n;
+while(n!=0)
+{
+rem=n%10;
+result=result+rem*rem*rem;
+n=n/10;
+}
+if(t==result)
+{
+printf("armstrong number between two intervals are %d ",result);
+}
+}
+}
+}
