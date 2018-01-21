4a2743d4d2bfcbef114c916bf54d5e2e2e288d34
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-21 09:42:35 +0530

    Update sumofarray.c

--- sumofarray.c
+++ sumofarray.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 int main()
 {
-int n,k,a[25],sum=0;
+int n,k,a[25],sum=0,i,j;
 printf("enter the numbers:");
-scanf("%d\t%d",&n,&k);
+scanf("%d",&n);
+printf("enter the values");
 for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
-for(i=0;i<k;i++)
+printf("no. of elements to be added");
+scanf("%d",&k);  
+for(j=0;i<j;j++)
 {
-sum=sum+a[i];
+sum=sum+a[j];
 }
 printf("sum is %d",sum);
 return 0;
