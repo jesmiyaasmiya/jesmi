19718aafa1704a2fb2a88d74f5b1194c6b6c13c4
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-21 12:10:36 +0530

    Create 3.minelement.c

---
+++ 3.minelement.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,a[50],min,i;
+	scanf("%d",&n);
+  scanf("%d",&a[1);
+  for(i=2;i<=n;i++)
+	{
+		{
+		scanf("%d",&a[i]);
+	  }
+		if(min>=a[i])
+		{
+			min=a[i];
+		}
+}
+       printf("%d",min);
+       return 0;
+}
