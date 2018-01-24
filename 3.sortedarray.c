0dc1d982f4d8f4d67d61d03394e3dd3e351c8d3d
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-24 16:21:45 +0530

    Update 3.sortedarray.c

--- 3.sortedarray.c
+++ 3.sortedarray.c
@@ -1,26 +1,22 @@
 #include<stdio.h>
 int main()
 {
-   int n,a[100],i,temp,j,k;
-   printf("enter the count\nenter the elements");
-   scanf("%d",&n);
-   for(i=1;i<=n;i++)
-   {
-       scanf("%d",&a[i]);
-   }
-   for(j=1;j<=n;j++)
-   {
-      for(k=j+1;k<=n;k++)
-      {
-        if(a[j]>=a[k])
-        {
-        temp=a[j];
-        a[j]=a[k];
-        a[k]=temp;
-            
-        }
-      }
-    printf(" %d\t",a[j]);
-   }
-    
+int i,j, a[90],n,temp;
+printf("enter no ");
+scanf("%d",&n);
+printf("enter array");
+for(i=1;i<=n;i++)
+scanf("%d",&a[i]);
+for(i=1;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+for(i=1;i<=n;i++)
+printf("%d ",a[i]);
+return 0;
 }
