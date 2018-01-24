6fb53cf38f101194207bc48a78361a8d366ba547
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-24 16:12:54 +0530

    Update 3.minelement.c

--- 3.minelement.c
+++ 3.minelement.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	int n,a[50],min,i;
+	int a[25],i,n,min;
+	printf("enter the number of elements:");
 	scanf("%d",&n);
-  scanf("%d",&a[1);
-  for(i=2;i<=n;i++)
+	printf("enter the numbers:\n");
+	scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
 	{
-		{
 		scanf("%d",&a[i]);
-	  }
-		if(min>=a[i])
+		min=a[0];
+	}
+	for(i=1;i<n;i++)
+	{
+		if(a[i]<min)
 		{
 			min=a[i];
 		}
+	}
+	printf("minimum element is %d",min);
+	return 0;
 }
-       printf("%d",min);
-       return 0;
-}
+	
+	
