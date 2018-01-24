e35d463ede07eefb7d13aa1a7424007aa2160743
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-24 16:55:05 +0530

    Update 3.27.c

--- 3.27.c
+++ 3.27.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-char a;
-scanf("%s",&a);
-if(a>='0'&&a<='9')
+	char j[10];
+	int i,m,count=0;
+	scanf("%s",j);
+	m=strlen(j);
+	for(i=0;i<m;i++)
+	{
+	if(j[i]=='1'||j[i]=='2'||j[i]=='3'||j[i]=='4'||j[i]=='5'||j[i]=='6'||j[i]=='7'||j[i]=='8'||j[i]=='9'||j[i]=='0')
 {
-printf("yes numeric",a);        
-}
-else
-{
-printf("no not numeric",a);
+	count++;
 }
 }
+	if(count==0)
+	{
+	printf(" not a numeric number");
+	}
+	else
+	printf(" numeric number");
+  return 0;
+	}
