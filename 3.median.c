21d0900d50941ae3d18b6422c161045b8a2008b7
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-28 17:43:36 +0530

    Update 3.median.c

--- 3.median.c
+++ 3.median.c
@@ -1,27 +1,30 @@
 #include<stdio.h>
 int main()
 {
-   int n,a[100],i,temp,j,k;
-   printf("enter the count\nenter the elements");
-   scanf("%d",&n);
-   for(i=1;i<=n;i++)
-   {
-       scanf("%d",&a[i]);
-   }
-   for(j=1;j<=n;j++)
-   {
-      for(k=j+1;k<=n;k++)
-      {
-        if(a[j]>=a[k])
-        {
-        temp=a[j];
-        a[j]=a[k];
-        a[k]=temp;
-            
-        }
-      }
-   
-   }
-     printf(" %d\t",a[j/2]);
-     return 0;
+int i,j,temp,n,a[150];
+printf("Enter the number:\n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(j=0;j<n;j++)
+{
+printf("%d,",a[j]);
+}
+printf("\nTo find the median:");
+n=(n-1)/2;
+printf("%d",a[n]);
 }
