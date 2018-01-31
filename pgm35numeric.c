8d8ed846dba3c6913d6387fba32571c4369587a9
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-31 19:41:41 +0530

    Create pgm35numeric.c

---
+++ pgm35numeric.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100];
+int i,n=0,len;
+printf("enter string:");
+scanf("%[\n]s",&a);
+for(i=0;i<len;i++)
+{
+if((a[i]>='0')||(a[i]<='9'))
+{
+n++;
+}
+printf("no. of numeric characters is %d",n);
+return 0;
+}
+
+
