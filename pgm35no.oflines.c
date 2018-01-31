c60aceba4d413a0eb120aa36ef971d82141e6116
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-01-31 15:51:33 +0530

    Create pgm35no.oflines.c

---
+++ pgm35no.oflines.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,count=0,lines;
+char a[100];
+printf("enter sentence:\n");
+scanf("%[^\n]s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+if((a[i]=='.')||(a[i]=='\n'))
+{
+count=count+1;
+}
+}
+lines=1+count;
+printf("no. of lines: %d",lines);
+return 0;
+}
