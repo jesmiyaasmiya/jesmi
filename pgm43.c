b7f1a6207575e7663d6f21b035de701ce6e9ee23
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-02-01 19:51:14 +0530

    Create pgm43.c

---
+++ pgm43.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char a[25],b[25];
+   printf("enter two string");
+   scanf("%s%s",&a,&b);
+   printf("%s%s",a,b);
+    
+
+        return 0;
+}
