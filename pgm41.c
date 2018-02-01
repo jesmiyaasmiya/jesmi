29bf453ead794d9a66b7a6f6661fe6dbde12f52e
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-02-01 19:23:33 +0530

    Create pgm41.c

---
+++ pgm41.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int k,i;
+    char a[50];
+    scanf("%d",&k);
+    scanf("%s",&a);
+    for(i=0;i<k;i++)
+    {
+        printf("%s\n",a);
+    }
+        return 0;
+}
