aa916b967d6adb5dcf6363dd56ff47bd9acb5db2
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-02-02 16:22:04 +0530

    Create pgm45.c

---
+++ pgm45.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the number:\n");
+	scanf("%d",&n);
+
+	
+		if((n>=0)&&(n<=10))
+		{
+			printf("yes");
+		}
+		else
+		{
+		printf("no");	
+		}
+	
+	return 0;
+}
