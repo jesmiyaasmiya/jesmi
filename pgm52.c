f199e20a45820e55749a84a74cb364daaf6876ea
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-02-19 15:51:09 +0530

    Create pgm52.c

---
+++ pgm52.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int num;
+scanf("%d",&num);
+switch(num)
+{
+case 1:printf("One");
+break;
+case 2:printf("Two");
+break;
+case 3:printf("Three");
+break;
+case 4:printf("Four");
+break;
+case 5:printf("Five");
+break;
+case 6:printf("Six");
+break;
+case 7:printf("Seven");
+break;
+case 8:printf("Eight");
+break;
+case 9:printf("Nine");
+break;
+case 10:printf("Ten");
+break;
+default:printf("");
+}
+return 0;
+}
