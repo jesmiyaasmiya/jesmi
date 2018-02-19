623f65429c22a4d75fa677a990ecc2f77b38b8b2
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-02-19 16:50:19 +0530

    Create pgm61.c

---
+++ pgm61.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int n,i;
+char a[25],b[25];
+printf("enter the string:\n");
+scanf("%s",&a);
+printf("to be displayed\n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+b[i]=a[i];
+}
+printf("%s",b);
+return 0;
+}
+
+
