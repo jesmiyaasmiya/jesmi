ba8980e8c962ded5491a098ee1b873fccfd53090
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-02-21 16:50:17 +0530

    Update pgm61.c

--- pgm61.c
+++ pgm61.c
@@ -1,19 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<string.h>
 int main()
 {
-int n,i;
-char a[25],b[25];
-printf("enter the string:\n");
-scanf("%s",&a);
-printf("to be displayed\n");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-b[i]=a[i];
+	char a[100],b[100];
+	int i,n;
+	printf("\nEnter character");
+	scanf("%s",&a);
+	printf("\nto be displayed");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		b[i]=a[i];
+	}
+	printf("\ncharacter is %s ",b);
+	return 0;
 }
-printf("\n character is %s",b);
-return 0;
-}
-
-
