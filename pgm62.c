fdfb0a934c1d05705cf03395a5e8860ffc476a6a
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-02-26 16:01:39 +0530

    Create pgm62.c

---
+++ pgm62.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[100],i;
+printf("enter the string\n");
+scanf("%s",&a);
+if(a[i]=='1'||a[i]=='0')
+{
+printf("yes, it is in binary representation");
+}
+else
+{
+printf("not binary");
+}
+return 0;
+}
