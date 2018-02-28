eb89c08d8dbfa3785d7efa8455e575abea268437
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-02-28 15:53:13 +0530

    Create pgm71.c

---
+++ pgm71.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[20],b[20];
+int c;
+printf("enter the string\n");
+scanf("%s",&a);
+strcpy(b,a);
+strrev(b);
+c=strcmp(a,s);
+if(c==0)
+{
+printf("given string is palindrome");
+}
+else
+{
+printf("given string is not a palindrome")]
+}
+return 0;
+}
