753c6fb99117595656b6ab29dc35be8800dfcc79
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-03-11 16:53:51 +0530

    Create pgm84.c

---
+++ pgm84.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("enter the character:\n");
+    scanf("%c",&a);
+    printf("The ASCII value for given character is %d",a);
+    return 0;
+}
