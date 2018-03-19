ff4ba19140c242208acf9f26310cdfadade94f26
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-03-19 16:08:56 +0530

    Create pgm81.c

---
+++ pgm81.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+int i,n,a,b,d;
+printf("enter the no of lines:\n");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+	scanf("%d%d",&a,&b);
+    d=b-a;
+    printf("%d\n",d);
+	
+}
+	return 0;
+}
