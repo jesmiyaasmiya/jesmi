eb95903de43f5117a3f406dd71d0fa763dd61353
jesmiyaasmiya <32517202+jesmiyaasmiya@users.noreply.github.com>
2018-03-19 16:16:35 +0530

    Create pgm83.c

---
+++ pgm83.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+	int a,b,c,d,e,f;
+	printf("\n enter the number");
+	scanf("%d %d",&a,&b);
+	e=a/b;
+	printf("\n enter te number");
+	scanf("%d %d",&c,&d);
+	f=c%d;
+	printf("the division is %d",e);
+	printf("the modulo is %d",f);
+	
+}
